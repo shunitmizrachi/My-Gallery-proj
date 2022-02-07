e3d2908d07c32046edd4564348f613cdd2aa2c01
Shunit Mizrachi <shuna1800@gmail.com>
2022-02-07 20:40:22 +0200

    added array of my projects

--- js/main.js
+++ js/main.js
@@ -1,2 +1,40 @@
 'use strict'
 
+const gProjs = [
+    {
+        id: "touch-nums",
+        name: "touch-nums",
+        title: "Touch The Numbers",
+        desc: "Touch The Numbers is a simple game for training your reflexes and peripheral vision.",
+        url: "projs/touch-nums",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "ball-board",
+        name: "ball-board",
+        title: "Ball On Board",
+        desc: "The aim of the game is to clear all the balls from the board.",
+        url: "projs/touch-nums",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "mine-sweeper",
+        name: "mine-sweeper",
+        title: "Mine Sweeper",
+        desc: "The objective of the game is to clear a rectangular board containing hidden mines without detonating any of them, with help from clues about the number of neighbouring mines in each field.",
+        url: "projs/mine-sweeper",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    },
+    {
+        id: "book-shop",
+        name: "book-shop",
+        title: "Book Shop",
+        desc: "The store owner can manage his books efficiently.",
+        url: "projs/book-shop",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    }
+]
